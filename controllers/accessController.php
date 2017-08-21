9c42fa2c3a171c3fbd4cccfdd9e15ad814254100
sergiosmam <sergiom_malarcon@yahoo.com>
2017-08-21 18:47:50 -0400

    Formulario de registro
    Se realizo la codificación del formulario de registro de nuevos usuarios, se codifico los inserts y los select principales del modelo de usuarios, información de usuarios y perfiles.

--- controllers/accessController.php
+++ controllers/accessController.php
@@ -6,6 +6,7 @@ class accessController extends IdEnController
                 parent::__construct();
             
                 $this->vUsersData = $this->LoadModel('users');
+                $this->vProfileData = $this->LoadModel('profile');
 			}
 			
 		public function index(){
@@ -27,22 +28,28 @@ class accessController extends IdEnController
     
 		public function RegisterMethod(){
                 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                    $vNames = (string) $_POST['vName'];
-                    $vLastNames = (string) $_POST['vLastNames'];
-                    $vEmail = (string) $_POST['vEmail'];
+                    $vNames = (string) strtolower($_POST['vName']);
+                    $vLastNames = (string) strtolower($_POST['vLastNames']);
+                    $vEmail = (string) strtolower($_POST['vEmail']);
                     $vPassword = (string) $_POST['vPassword'];
                     $vRePassword = (string) $_POST['vRePassword'];
                     $vRole = (string) 'user';
                     $vActivationcode = rand(10000, 99999);
                     $vActive = (int) 2;
-                                        
+                    
                     if(($this->vUsersData->getUserNameExists($vEmail) == 0) && ($this->vUsersData->getUserEmailExists($vEmail) == 0)){
                         $vUserCode = $this->vUsersData->userRegister($vEmail, $vPassword, $vRePassword, $vEmail, $vRole, $vActivationcode, $vActive);
                         if($vUserCode != 0){
+                            $vOthername = (string) $vEmail;
                             $UserNameCode = $this->vUsersData->userInfoRegister($vUserCode, $vNames, $vLastNames, $vOthername, $vBirthDate, $vCountry, $vCity, $vActive);
-                            /*if($UserNameCode != 0){
-                                $UserNameCode = $this->vUsersData->userInfoRegister($vUserCode, $vNames, $vLastNames, $vOthername, $vBirthDate, $vCountry, $vCity, $vActive);
-                            } */                           
+                            if($UserNameCode != 0){
+                                $vProfileName = (string) strtolower(str_replace(' ', '', $vNames).str_replace(' ', '', $vLastNames));
+                                $vProfileType = 1;
+                                $ProfileCode = $this->vProfileData->profileRegister($vUserCode, $vProfileName, $vProfileType, $vActive);
+                                if(($vUserCode != 0) && ($UserNameCode != 0) && ($ProfileCode != 0)){
+                                    echo 'El usuario se registro correctamente!';
+                                }
+                            }
                         }
                     } else {
                         echo 0;
